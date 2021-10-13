1a8cb2275ad8fbad0848f4df62d3010f6f5a1e70
AlenKraemer <90884647+AlenKraemer@users.noreply.github.com>
2021-10-13 19:25:02 -0300

    ScoreManager
    Agregada condicion de victoria y derrota

--- Proyect-S/Assets/_Main/_Scripts/ScoreManager.cs
+++ Proyect-S/Assets/_Main/_Scripts/ScoreManager.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class ScoreManager : MonoBehaviour
 {
+    public static ScoreManager Instance;
     private Text scoreText;
     private int scoreIncrease = 1;
+    private int totalScore;
+    private LifeController lifeController;
+    [SerializeField] private int filesQuantity;
+    [SerializeField] private GameObject player;
 
     private void Awake()
     {
+        Instance = this;
         scoreText = GetComponent<Text>();
+        lifeController = player.GetComponent<LifeController>();
+        scoreText.text = $"{totalScore}/{filesQuantity}";
+    }
+
+    private void Update()
+    {
+        if(totalScore == filesQuantity)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if(lifeController.CurrentHealth <= 0)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
     public void AddScore()
     {
-        scoreText.text = $"{scoreIncrease}/3";
-        scoreIncrease++;
+        totalScore += scoreIncrease;
+        scoreText.text = $"{totalScore}/{filesQuantity}";
+        
     }
 }
