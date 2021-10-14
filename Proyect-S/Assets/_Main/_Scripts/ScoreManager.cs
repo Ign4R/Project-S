d69dd14c7173c093a280ebd053d89a73334de0ac
AlenKraemer <90884647+AlenKraemer@users.noreply.github.com>
2021-10-13 21:23:14 -0300

    CountDownTimer
    Un timer aparece al cumplir la condicion de los files, si llega a 0 el jugador pierde.

--- Proyect-S/Assets/_Main/_Scripts/ScoreManager.cs
+++ Proyect-S/Assets/_Main/_Scripts/ScoreManager.cs
@@ -8,6 +8,9 @@ public class ScoreManager : MonoBehaviour
     private int scoreIncrease = 1;
     private int totalScore;
     private LifeController lifeController;
+    private bool starTimer;
+    [SerializeField] private GameObject timerText;
+    [SerializeField] private float timeValue = 90;
     [SerializeField] private int filesQuantity;
     [SerializeField] private GameObject player;
 
@@ -21,15 +24,50 @@ public class ScoreManager : MonoBehaviour
 
     private void Update()
     {
+        if(starTimer == true)
+        {
+
+            if (timeValue > 0)
+            { 
+                timeValue -= Time.deltaTime;
+            }
+
+            if(timeValue <= 30)
+            {
+                timerText.GetComponent<Text>().color = Color.red;
+            }
+
+            if(timeValue <= 0)
+            {
+                timeValue = 0;
+                SceneManager.LoadScene(2);
+            }
+
+            DisplayTime(timeValue);
+        }
+
         if(totalScore == filesQuantity)
         {
-            SceneManager.LoadScene(1);
+            starTimer = true;
+            timerText.SetActive(true);
         }
 
         if(lifeController.CurrentHealth <= 0)
         {
             SceneManager.LoadScene(2);
         }
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.GetComponent<Text>().text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     public void AddScore()
