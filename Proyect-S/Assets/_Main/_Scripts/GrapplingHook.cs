804e5ff86668aa57d81d2880d21bfac0cb4d8c2e
ignisR <ignaciodanielreynaga@gmail.com>
2021-10-22 19:51:10 -0300

    add cooldown hook

--- Proyect-S/Assets/_Main/_Scripts/GrapplingHook.cs
+++ Proyect-S/Assets/_Main/_Scripts/GrapplingHook.cs
@@ -5,41 +5,40 @@ using UnityEngine;
 public class GrapplingHook : MonoBehaviour
 {
     private LineRenderer hookLine;
-    private Vector3 hookDir;
     private Vector3 grapplePoint;
-    private float distanceFromPoint;
-    [SerializeField]
-    private float maxDistance = 40f;
     private float currentDistance;
-    public float distanceRange;
-    public float hookSpeed;
-    public LayerMask toGrapple;
-    public Transform hook;
-    public Transform cameraTransform;
-    public Transform player;
-    private Rigidbody rb;
 
-    public bool check;
-    public float force;
-    public float forceMultiplier;
-    public float velocityForce;
-    private float distanceDiference;
+    [SerializeField] private float maxDistance = 40f;  
+    [SerializeField] private float distanceRange;
+    [SerializeField] private float hookSpeed;
+    [SerializeField] private Transform cameraTransform;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float cooldown;
+
+    private float currentCooldown;
+    private Rigidbody rb;
+    private bool check;
 
     void Awake()
     {
         hookLine = GetComponent<LineRenderer>();
-        rb = player.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        playerTransform = GetComponent<Transform>();
     }
 
     void Update()
     {
-
+        if (currentCooldown >= 0)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
     }
 
     private void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {         
+        if (Input.GetKeyDown(KeyCode.E) && currentCooldown <= 0) 
+        {
+            currentCooldown = cooldown;
             StartGrapple();
         }
         else if (Input.GetKeyUp(KeyCode.E))
@@ -52,7 +51,7 @@ public class GrapplingHook : MonoBehaviour
         }
         if (check == true)
         {
-            currentDistance = Vector3.Distance(player.position, grapplePoint);
+            currentDistance = Vector3.Distance(playerTransform.position, grapplePoint);
             if(currentDistance > distanceRange)
             {
                 transform.position = Vector3.MoveTowards(transform.position, grapplePoint, hookSpeed * Time.deltaTime);
@@ -76,7 +75,6 @@ public class GrapplingHook : MonoBehaviour
             rb.velocity = Vector3.zero;
             check = true;
             grapplePoint = hit.point;
-            hookDir = (grapplePoint - transform.position).normalized;
             hookLine.positionCount = 2;
             Physics.gravity = Vector3.zero;
         }
@@ -94,9 +92,8 @@ public class GrapplingHook : MonoBehaviour
     void DrawRope()
     {
         if (check == false) return;
-        hookLine.SetPosition(index: 0, hook.position);
+        hookLine.SetPosition(index: 0, playerTransform.position);
         hookLine.SetPosition(index: 1, grapplePoint);
-
 
     }
 }
