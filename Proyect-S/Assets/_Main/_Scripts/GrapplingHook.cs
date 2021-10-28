688ea3990a0b11556dca5eadff6d081a5986c4a6
ignisR <ignaciodanielreynaga@gmail.com>
2021-10-27 22:52:23 -0300

    add new player

--- Proyect-S/Assets/_Main/_Scripts/GrapplingHook.cs
+++ Proyect-S/Assets/_Main/_Scripts/GrapplingHook.cs
@@ -36,12 +36,12 @@ public class GrapplingHook : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.E) && currentCooldown <= 0) 
+        if (Input.GetKeyDown(KeyCode.Z) && currentCooldown <= 0) 
         {
             currentCooldown = cooldown;
             StartGrapple();
         }
-        else if (Input.GetKeyUp(KeyCode.E))
+        else if (Input.GetKeyUp(KeyCode.Z))
         {
             Physics.gravity = new Vector3(0f, -9.81f, 0f);
             rb.constraints = RigidbodyConstraints.None;
